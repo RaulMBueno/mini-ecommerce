847abcf018487c82d5236eae3ad2f645e3b6d23f
Raul Martins Bueno <raulmartinsbueno@gmail.com>
2025-09-22 20:10:24 -0300

    feat: add Product controller and GET endpoint

---
+++ src/main/java/com/raulmbueno/mini_ecommerce/controllers/ProductController.java
@@ -0,0 +1,25 @@
+package com.raulmbueno.mini_ecommerce.controllers;
+
+import com.raulmbueno.mini_ecommerce.entities.Product;
+import com.raulmbueno.mini_ecommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
