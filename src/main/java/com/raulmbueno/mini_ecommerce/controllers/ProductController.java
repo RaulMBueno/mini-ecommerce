706607567c6b55b27877e098199a4b7b96add66c
Raul Martins Bueno <raulmartinsbueno@gmail.com>
2025-09-22 20:57:36 -0300

    feat: add POST endpoint to create new products

--- src/main/java/com/raulmbueno/mini_ecommerce/controllers/ProductController.java
+++ src/main/java/com/raulmbueno/mini_ecommerce/controllers/ProductController.java
@@ -5,6 +5,8 @@ import com.raulmbueno.mini_ecommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,11 @@ public class ProductController {
     public ResponseEntity<List<Product>> findAll() {
         List<Product> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> insert(@RequestBody Product product) {
+        Product newProduct = service.save(product);
+        return ResponseEntity.status(201).body(newProduct);
     }
 }
