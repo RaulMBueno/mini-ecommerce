bbce8d0d8bc849ee7d7749b1f1e164eda747af61
Raul Martins Bueno <raulmartinsbueno@gmail.com>
2025-09-24 14:32:34 -0300

    feat: add ProductDTO for data transfer

---
+++ src/main/java/com/raulmbueno/mini_ecommerce/dtos/ProductDTO.java
@@ -0,0 +1,40 @@
+package com.raulmbueno.mini_ecommerce.dtos;
+
+import com.raulmbueno.mini_ecommerce.entities.Product;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class ProductDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public ProductDTO(Product entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.price = entity.getPrice();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
