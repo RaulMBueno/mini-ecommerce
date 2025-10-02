8cc9d909c2520baf6446a8a36044b329dc4b1b42
Raul Martins Bueno <raulmartinsbueno@gmail.com>
2025-10-02 08:49:22 -0300

    feat: Create and refactor repository layer

--- src/main/java/com/raulmbueno/mini_ecommerce/repositories/UserRepository.java
+++ src/main/java/com/raulmbueno/mini_ecommerce/repositories/UserRepository.java
@@ -2,11 +2,16 @@ package com.raulmbueno.mini_ecommerce.repositories;
 
 import com.raulmbueno.mini_ecommerce.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // MUDANÇA: Spring Data JPA implementa este método automaticamente!
-    User findByEmail(String email); 
+    /**
+     * Busca um usuário pelo seu endereço de e-mail, que é usado como username.
+     *
+     * @param email O e-mail do usuário a ser buscado.
+     * @return um Optional contendo o User, se encontrado, ou um Optional vazio caso contrário.
+     */
+    Optional<User> findByEmail(String email);
 }
