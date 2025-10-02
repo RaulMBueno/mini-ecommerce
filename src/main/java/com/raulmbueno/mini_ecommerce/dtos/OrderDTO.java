344a66059ae9a84883be517c45d7571d15557057
Raul Martins Bueno <raulmartinsbueno@gmail.com>
2025-10-01 23:35:40 -0300

    refactor: Adapt DTO and Service to use OrderStatus enum

--- src/main/java/com/raulmbueno/mini_ecommerce/dtos/OrderDTO.java
+++ src/main/java/com/raulmbueno/mini_ecommerce/dtos/OrderDTO.java
@@ -1,47 +1,29 @@
 package com.raulmbueno.mini_ecommerce.dtos;
 
 import com.raulmbueno.mini_ecommerce.entities.Order;
+import com.raulmbueno.mini_ecommerce.entities.enums.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.time.Instant;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
     private Instant moment;
-    private Integer orderStatus;
+    private OrderStatus orderStatus; // Corrigido de Integer para OrderStatus
     private ClientDTO client;
-
-    public OrderDTO() {
-    }
-
-    public OrderDTO(Long id, Instant moment, Integer orderStatus, ClientDTO client) {
-        this.id = id;
-        this.moment = moment;
-        this.orderStatus = orderStatus;
-        this.client = client;
-    }
 
     public OrderDTO(Order entity) {
         this.id = entity.getId();
         this.moment = entity.getMoment();
-        this.orderStatus = entity.getOrderStatus();
+        this.orderStatus = entity.getOrderStatus(); // Agora os tipos batem perfeitamente
         this.client = new ClientDTO(entity.getClient());
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Instant getMoment() {
-        return moment;
-    }
-
-    public Integer getOrderStatus() {
-        return orderStatus;
-    }
-
-    public ClientDTO getClient() {
-        return client;
     }
 }
--- src/main/java/com/raulmbueno/mini_ecommerce/services/OrderService.java
+++ src/main/java/com/raulmbueno/mini_ecommerce/services/OrderService.java
@@ -1,24 +1,28 @@
 package com.raulmbueno.mini_ecommerce.services;
 
 import com.raulmbueno.mini_ecommerce.dtos.OrderDTO;
+import com.raulmbueno.mini_ecommerce.entities.Client;
 import com.raulmbueno.mini_ecommerce.entities.Order;
-import com.raulmbueno.mini_ecommerce.entities.Client; 
+import com.raulmbueno.mini_ecommerce.entities.enums.OrderStatus;
+import com.raulmbueno.mini_ecommerce.repositories.ClientRepository;
 import com.raulmbueno.mini_ecommerce.repositories.OrderRepository;
-import com.raulmbueno.mini_ecommerce.repositories.ClientRepository; 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
 
-    @Autowired
-    private OrderRepository orderRepository;
+    private final OrderRepository orderRepository;
+    private final ClientRepository clientRepository;
 
-    @Autowired 
-    private ClientRepository clientRepository;
+    public OrderService(OrderRepository orderRepository, ClientRepository clientRepository) {
+        this.orderRepository = orderRepository;
+        this.clientRepository = clientRepository;
+    }
 
     @Transactional(readOnly = true)
     public List<OrderDTO> findAll() {
@@ -29,9 +33,10 @@ public class OrderService {
     @Transactional
     public OrderDTO insert(OrderDTO dto) {
         Order entity = new Order();
-        entity.setMoment(dto.getMoment());
-        entity.setOrderStatus(dto.getOrderStatus());
-        
+
+        entity.setMoment(Instant.now());
+        entity.setOrderStatus(OrderStatus.WAITING_PAYMENT);
+
         Client client = clientRepository.getReferenceById(dto.getClient().getId());
         entity.setClient(client);
 
