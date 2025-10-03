666bd4b707083e1fd7623ead94123ee9aedeba81
Raul Martins Bueno <raulmartinsbueno@gmail.com>
2025-10-02 23:54:06 -0300

    feat: Migrate database from H2 to PostgreSQL with Flyway

--- src/main/java/com/raulmbueno/mini_ecommerce/repositories/OrderRepository.java
+++ src/main/java/com/raulmbueno/mini_ecommerce/repositories/OrderRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    @Query("SELECT obj FROM Order obj JOIN FETCH obj.items item JOIN FETCH item.product WHERE obj.id = :id")
+    @Query("SELECT obj FROM Order obj JOIN FETCH obj.items item JOIN FETCH item.id.product WHERE obj.id = :id")
     Optional<Order> findOrderWithItemsById(Long id);
 }
