99dd842634efa299781a4a575ca3dcde32686813
Raul Martins Bueno <raulmartinsbueno@gmail.com>
2025-11-25 14:06:16 -0300

    fix: Correção completa da segurança JWT, Entidade User e permissões

--- src/main/java/com/raulmbueno/mini_ecommerce/config/security/SecurityFilter.java
+++ src/main/java/com/raulmbueno/mini_ecommerce/config/security/SecurityFilter.java
@@ -2,12 +2,11 @@ package com.raulmbueno.mini_ecommerce.config.security;
 
 import com.raulmbueno.mini_ecommerce.repositories.UserRepository;
 import com.raulmbueno.mini_ecommerce.services.TokenService;
-
-import jakarta.annotation.Nonnull;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -28,7 +27,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(@Nonnull HttpServletRequest request,@Nonnull HttpServletResponse response,@Nonnull FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
 
         if (token != null) {
